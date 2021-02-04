507a3f65f9d94341c126caab129f51e7fb322c91
932824098@qq.com <932824098@qq.com>
2021-02-04 14:14:00 +0800

    文件夹建立

---
+++ src/main/java/pers/xiaobaobao/bigwatermelon/BigWaterMelon.java
@@ -0,0 +1,8 @@
+package pers.xiaobaobao.bigwatermelon;
+
+/**
+ * @author bao meng yang <932824098@qq.com>
+ * @date 2021/2/4，14:13:02
+ */
+public class BigWaterMelon {
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+/**
+ * @author bao meng yang <932824098@qq.com>
+ * @date 2021/2/4，14:13:15
+ */
+public class Test {
+}
